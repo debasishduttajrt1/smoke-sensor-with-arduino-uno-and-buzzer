18f7dd71fc584b749743e5242229ea07ed0803bb
debasish dutta <47662257+debasishduttajrt1@users.noreply.github.com>
2019-03-08 23:08:14 +0530

    Add files via upload

---
+++ arduino1.ino
@@ -0,0 +1,32 @@
+
+void setup() {
+ 
+  Serial.begin(9600);
+  pinMode(A0,INPUT);
+  pinMode(7,OUTPUT);
+  
+
+
+}
+
+void loop() {
+ int a=analogRead(A0);
+ Serial.println(a);
+  delay(100);
+  if(a>60) { 
+  
+   digitalWrite(7,HIGH);
+   delay(1000);
+   
+
+    
+  }
+  else{
+ digitalWrite(7,LOW);
+    }
+  
+}
+
+ 
+
+   
